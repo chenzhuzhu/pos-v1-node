0538bd9a4e64b57cc1f049c2cd6b12ef28e091b3
陈昱竹 <826537618@qq.com>
2017-08-09 04:27:04 +0800

    has problem

--- main/main.js
+++ main/main.js
@@ -54,10 +54,10 @@ function countShoppingList(shopping_list){
       
       let temp_list =expand(shopping_list);
       let counted_list =toCountList(temp_list);    
-      console.log(counted_list);
-      return counted_list;            
+      // console.log(counted_list);
+      return counted_list;
 }
-      
+
 function findItemInfo(collection,item){
       for(let each_info of collection){
             if(each_info['barcode'] == item){
@@ -78,53 +78,157 @@ function getListInfo(counted_list){
                   new_arr.push(finded_info);
             }
       }
-      console.log(new_arr);
+      // console.log(new_arr);
       return new_arr;           
 }
 
 function findPromotionInfo(collection,item){
       for(let each of collection){
+
             if(each==item['barcode']){
                   return item
             }
       }
       return false
 }
+
+function hasFree(item){
+      return parseInt(item/2,10)
+
+}
+function BuyTwoGetOneFree(temp_arr){
+      let replace_arr = [];
+
+      for(let each_temp_arr of temp_arr){
+            let barcode =each_temp_arr.barcode
+            let name =each_temp_arr.name
+            let price =each_temp_arr.price
+            let count =each_temp_arr.count
+            let unit = each_temp_arr.unit
+            let every_new ={barcode,name,unit,price,count}
+            replace_arr.push(every_new)
+      }
+      for(let each_replace of replace_arr){
+            each_replace.count = hasFree(each_replace.count)
+      }
+      return replace_arr;
+
+
+}
 //5# 依据loadPromotions()中return的内容，得出优惠的内容
 function countPromotions(list_detail_info){
-      let new_arr =[];
+      let promotion_arr;
+      let temp_arr=[];
       let promotion_list = datbase.loadPromotions()  //6#
       for(let each_detail_info of list_detail_info){
-            let finded_promotion = findPromotionInfo(promotion_list['barcodes'],each_detail_info)
+            let finded_promotion = findPromotionInfo(promotion_list[0]['barcodes'],each_detail_info)
             if (finded_promotion){
-                  new_arr.push();
+                  temp_arr.push(each_detail_info);
             }
       }
-      console.log(new_arr);
-      return new_arr;
-      
+      promotion_arr = BuyTwoGetOneFree(temp_arr);
+
+      return promotion_arr;
+
 }
 
-function sumAll(){
-      
+function findReduce(collection,item){
+      for(let each of collection){
+            if (each.name == item.name){
+                  return each
+            }
+      }
+      return false
+}
+//#7 总计购买所有东西的金额
+//输出：
+// '***<没钱赚商店>购物清单***\n' +
+// '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
+// '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
+// '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
+// // '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
+function sumAll(all_collection,promotion_collection){
+      let collection=[];
+      for(let all_item of all_collection){
+            let to_reduce =findReduce(promotion_collection,all_item)
+            if(to_reduce){
+                  let name=all_item.name
+                  let count=all_item.count
+                  let unit = all_item.unit
+                  let price =all_item.price
+                  let total =(all_item.count-to_reduce.count)*all_item.price
+                  let new_reduce_result ={name,count,unit,price,total}
+                  collection.push(new_reduce_result)
+            }else{
+                  all_item.total =all_item.count*all_item.price
+                  collection.push(all_item)
+
+            }
+
+      }
+
+      let print_part1 ='***<没钱赚商店>购物清单***\n';
+      for(let item of collection){
+            print_part1 +='名称：'+item.name+'，数量'+item.count+item.unit+'，单价:'+item.price.toFixed(2)+'(元)，小计：'+item.total.toFixed(2)+'(元)\n'
+      }
+
+      return print_part1
 }
 
-function sumPromotion(){
-      
+// #8 总计买二赠一的东西
+// 输出：
+// '挥泪赠送商品：\n' +
+// '名称：雪碧，数量：1瓶\n' +
+// '名称：方便面，数量：1袋\n' +
+function sumPromotion(collection){
+      let print_part1 ='挥泪赠送商品：\n';
+      for(let item of collection){
+            print_part1 +='名称：'+item.name+'，数量：'+item.count+item.unit+'\n';
+      }
+      return print_part1
 }
 
-function sumMoney(){
-      
+function sumMoneyType(collection){
+      let sum=0 ;
+      for(let item of collection){
+            sum+=item.count*item.price;
+      }
+      return sum
+
 }
 
-function printResult(){
-      
+//#9 计价
+//输出
+//'总计：51.00(元)\n' +
+// '节省：7.50(元)\n' +
+function sumMoney(all,promotion){
+      let all_products_money = sumMoneyType(all);
+      let promotion_products_money=sumMoneyType(promotion);
+      let result ='总计：'+all_products_money+'(元)\n'+'节省：'+promotion_products_money+'(元)\n';
+      return result
+}
+
+function printResult(result_list_detail_info,result_promotion_list,all_type_money){
+      return result_list_detail_info+'----------------------\n' +result_promotion_list+'----------------------\n' +all_type_money+'**********************'
+
 }
 
 module.exports = function printInventory(inputs){
       let counted_list = countShoppingList(inputs);
-      let list_detail_info=getListInfo(counted_list);
-      c
+      let list_detail_info=getListInfo(counted_list);  //购买所有东西的详细信息
+      // let used_for_promotion =list_detail_info;
+      // console.log(list_detail_info)
+      let promotion_list = countPromotions(list_detail_info)
+      // console.log(promotion_list)
+      let result_list_detail_info = sumAll(list_detail_info,promotion_list)
+      // console.log(result_list_detail_info)
+      let result_promotion_list =sumPromotion(promotion_list)
+      // console.log(result_promotion_list)
+
+      let all_type_money=sumMoney(list_detail_info,promotion_list)
+      // console.log(all_type_money)
+
+      console.log(printResult(result_list_detail_info,result_promotion_list,all_type_money))
       
       
 };
