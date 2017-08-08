c304037c25b1b1a2d185cfbbba5e614b93a40972
yuzhu <yuzhucyz@163.com>
2017-08-08 02:05:06 +0000

    function2# fininshed

--- main/main.js
+++ main/main.js
@@ -1,15 +1,17 @@
 function expand(collection){
       let new_arr=[];
+      
       for(let each_item of collection){
             if (each_item.includes('-')){
-                  expanded_array = each_item.split('-');
+                  let expanded_array = each_item.split('-');
                   let key =expanded_array[0];
-                  let value = expand_array[1];
+                  let value = expanded_array[1];
                   push(new_arr,key,value);
             }else{
                   new_arr.push(each_item)
-            }
-      console.log(new_arr);
+            }     
+      }
+//      console.log(new_arr); 
       return new_arr;
 }
 
@@ -19,11 +21,38 @@ function push(result,key,value){
       }
       return result;      
 }
-function countShoppingLIst(shopping_list){
-      let counted_list=[];
-      let temp_list=[];
-      temp_list =expand(shopping_list);
-      return temp_list
+
+function toCountList(collection){
+      let new_arr=[];
+      for(let item of collection){
+            let obj = find(new_arr,item)
+            if(obj){
+                  obj.count++;
+            }else{
+                  new_arr.push({key:item,count:1})
+            }
+      }
+      console.log(new_arr);
+      return new_arr;
+}
+
+function find(collection,item){
+      for(let obj of collection){
+            if(obj.key ==item){
+                  return obj
+            }
+      }
+      return false;
+}
+
+
+//2# 
+function countShoppingList(shopping_list){
+      
+      let temp_list =expand(shopping_list);
+      let counted_list =toCountList(temp_list);
+      
+      return counted_list
 
             
 }
@@ -34,24 +63,28 @@ function getListInfo(){
       
       
 }
+
 function countPromotions(){
       
 }
+
 function sumAll(){
       
 }
+
 function sumPromotion(){
       
 }
+
 function sumMoney(){
       
 }
+
 function printResult(){
       
 }
 
-
-module.exports = function printInventory(inputs) {
-      countShoppingLIst(inputs);
+module.exports = function printInventory(inputs){
+      countShoppingList(inputs)
       
 };
